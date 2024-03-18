3dbccb616bdf644db84b32e94d4fc00acefd2b05
Kurt Isiah S. Santillana <157768770+Rsccccc@users.noreply.github.com>
2024-03-18 09:15:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,83 @@
-console.log("Hello World!");
+console.log("Hello World");
+let B = false,
+I = false,
+N = false,
+G = false,
+O = false;
 
-let B = false
-    I = false
-    N = false
-    G = false
-    O = false
+let BNumber, INumber, NNumber, GNumber, ONumber
+function newNumber(maxNumber) {
+    let x = Math.floor(Math.random() * maxNumber) + 1 ;
 
-function newNumber (maxNumber){
-    let x = Math.floor(Math.random() * maxNumber)+1; 
-    if (x) {} 
-    // if.. else if... else statement
-    //conditional operator
-    // GTE > = LTE < =, EQ =, NEQ! =, AND &&, or ||
-    if (x <= 15){ console.log (`New number ${x} belongs to "B"`); B = true;
-} else if (x >= 16 && x <= 30)
-{
-    console.log(`New Number ${x} belong to "I"`);
-     I = true;
-} else if (x >= 30 && x <= 45)
-{
-    console.log(`New Number ${x} belong to "N"`);
-     N = true;
+    // if ..... else if .... else statement
+    // Conditional operator:
+    // GTE > =, LTE < =, EQ ==, NEQ ! =, AND &&, OR ||
+    if(x <= 15){
+        console.log(`New number ${x} belongs to "B"`);
+        B = true;
+        BNumber =x;
+    } else if (x >= 16 && x <= 30) {
+        console.log(`New number ${x} belongs to "I"`);
+        I = true;
+        INumber = x;
+    } else if (x >= 31 && x <= 45) {
+        console.log(`New number ${x} belongs to N`);
+        N = true;
+        NNumber = x;``
+    } else if (x >= 46 && x <= 60) {
+        console.log(`New number ${x} belongs to G`);
+        G = true;
+        GNumber = x;
+    } else if (x >= 61 && x <= 75) {
+        console.log(`New number ${x} belongs to O`);
+        O = true;
+        ONumber = x;
+    } else {
+        console.log(`New number ${x} is invalid`);
+    }
+
+    
+    if (B == true && I == true && N == true && G == true && O == true){
+        console.log(`BINGO!!!!!`);
+        console.table({
+ B:BNumber,
+ I:INumber,
+ N:NNumber,
+ G:GNumber,
+ O:ONumber,
+        }
+
+        )
+    }
 }
-else if (x >= 45 && x <= 60)
-{
-    console.log(`New Number ${x} belong to "G"`);
-     G = true;
+
+//newNumber(75);
+
+// LOOPS 
+//for(let counter = 10; counter >= 0; counter += 1){
+ //   console.log(`Current counter is ${counter}`);
+//}
+
+let counter = 5;
+//for(counter; counter >= 0; counter--){
+//   console.log(`Current counter is ${counter}`);
+//   if (counter = 2) break;
+//
+//
+//newNumber(75);
+
+//}
+
+//while (counter >= 0) {
+//    console.log(`while, current counter is ${counter}`);
+//    if (counter=2) break;
+//
+//    newNumber(75);
+//    counter--;
+//}
+while (B == false || I == false || N == false || G == false || O == false){
+    
+    newNumber(75);
 }
-else if (x >= 60 && x <= 75)
-{
-    console.log(`New Number ${x} belong to "O"`);
-     O = true;
-}
-
-else {console. log(`New Number ${x} is invalid.`);
-
-}
-if (B == true && I == true && N == true && G == true && O == true){
-    console.log (`BINGGOOO!!`);
-  }
-}
-
-console.log(newNumber());
-
-
 
 
